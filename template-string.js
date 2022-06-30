77c579e2f728b95edd1e186e28e5b37bac78af49
JoaoPDeveloper <joao_entreprise@hotmail.com>
2022-06-29 23:04:26 -0300

    alterações

--- template-string.js
+++ template-string.js
@@ -2,14 +2,7 @@ let meuNome =  'Joao';
 let meuSobrenome = 'Pedro';
 let minhaProfissao = 'Help-desk';
 
-// sem template string
-
-// console.log('Olá, eu sou ' + meunome + '' + meuSobrenome + 'minha profissão é: ' + minhaProfissao);
-
-
-
 console.log(`Olá, eu sou ${meuNome} ${meuSobrenome} e minha profissão é : ${minhaProfissao}`) 
-
 
 console.log(`o resultado da soma de 1 + 1 =${1 + 1}`);
 
